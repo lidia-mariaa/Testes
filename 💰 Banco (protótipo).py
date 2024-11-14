44a85394c62ff36be018f682593c354c1ce1ae03
Lídia Maria <lidia.maria.lucas.lopes@gmail.com>
2024-11-14 09:50:15 -0300

    Create 💰 Banco (protótipo).py

---
+++ 💰 Banco (protótipo).py
@@ -0,0 +1,18 @@
+senha = "12345"
+usuario = "lidia"
+
+while True:
+    usuario2 = input("Informe o usuário: ")
+    senha2 = input("Informe a senha: ")
+    if senha2 != senha or usuario2 != usuario:
+        print("\nSenha incorreta ou usuário incorreto :C")
+        oque_quer = input("\nOque vc quer?\na)Reescrever\nb)Mudar a senha\n").lower()
+        if oque_quer == "b":
+            senha_nv = input("Informe a nova senha: ")
+            senha = ""
+            print (senha)
+            senha += senha_nv
+            print(senha)
+    else:
+        print("\nSenha e usuário correto!")
+        break
