8b090464bad562e888cac36aa9cb0f25d4787f76
Lídia Maria <lidia.maria.lucas.lopes@gmail.com>
2026-03-15 17:21:00 -0300

    Fix print statements and add midpoint calculation

--- 📈 Plano Cartesiano.py
+++ 📈 Plano Cartesiano.py
@@ -47,10 +47,32 @@ while True:
         
     #DISTÂNCIA ENTRE OS PONTOS
     distancia = ((abscissa_A - abscissa_B)**2 + (ordenada_A - ordenada_B)**2)**0.5
+
+    #PONTO MÉDIO
+    ponto_medio_x = ((abscissa_A + abscissa_B)/2)
+    ponto_medio_y = ((ordenada_A + ordenada_B)/2)
     
     #EXIBINDO INFORMAÇÕES
     print(f"\n{nome}, o ponto A({abscissa_A},{ordenada_A}) está no quadrante {quadrante_A}, e o ponto B({abscissa_B},{ordenada_B}) está no quadrante {quadrante_B}!")
-    print(f"E a distância entre esses ponto é: {distancia}")
+    print(f"A distância entre esses pontos é: {distancia}")
+    print(f"E o ponto médio entre esses pontos é: {ponto_medio_x},{ponto_medio_y}")
+
+    #PONTO 3
+    p3_decisão = input("Gostaria de saber se o ponto A e B estão alinhados com um ponto C? ").lower()
+
+    if p3_decisão == "sim":
+        abscissa_C = int(input("\nÓtimo!\nInforme a abscissa de C: "))
+        ordenada_C = int(input("Informe a ordenada de C: "))
+
+        #DETERMINANTE
+        determinante = abscissa_A * (ordenada_B - ordenada_C) + abscissa_B * (ordenada_C - ordenada_A) + abscissa_C * (ordenada_A - ordenada_B)
+
+        if determinante ==  0:
+            print("O ponto A, B, C estão alinhados!")
+        else:
+            print("O ponto A, B, C não estão alinhados.")
+    else:
+        pass
 
     #REINICIAR OU NÃO?
     reiniciar = input("\nOque você gostaria de fazer agora?\na)Sair\nb)Reiniciar\n")
